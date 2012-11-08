c272f14284f711c128809723b31b31bd9410b080
rsemlal <reda.semlal@gmail.com>
2012-11-08 21:06:23 +0100

    Ajout des exceptions

---
+++ scala-notes-core/src/main/scala/net/rsemlal/scalanotes/core/ScalaNoteExceptions.scala
@@ -0,0 +1,19 @@
+package net.rsemlal.scalanotes.core
+
+import net.rsemlal.scalanotes.core.data.NoteRef
+
+object ScalaNoteExceptions {
+  sealed abstract class ScalaNoteException(message: String, innerException: Throwable = null)
+    extends Exception(message, innerException)
+
+  sealed class EncryptionException(val noteRef: NoteRef, message: String, innerException: Throwable = null)
+    extends ScalaNoteException(message, innerException)
+
+  final class UnlockedTokenNeededException(_noteRef: NoteRef)
+    extends EncryptionException(_noteRef,
+      "La note '%s' est encryptée, elle nécessite un token déverouillé".format(_noteRef.name))
+
+  final class NoTokenNeededException(_noteRef: NoteRef)
+    extends EncryptionException(_noteRef,
+      "La note '%s' n'est pas encryptée, elle ne nécessite pas de token".format(_noteRef.name))
+}
